59ba41c2f168e5c0cbf39d0d2d23e24b7af7cce6
Aman Rao <124618381+Aman5012@users.noreply.github.com>
2025-09-13 19:17:03 +0530

    Add SQL script to delete duplicate emails

---
+++ 004. Delete Duplicate Emails.sql
@@ -0,0 +1,9 @@
+196. Delete Duplicate Emails
+
+//QUESTION LINK: https://leetcode.com/problems/delete-duplicate-emails/
+
+CODE:
+
+DELETE p1 FROM Person p1
+JOIN Person p2 
+ON p1.email = p2.email AND p1.id > p2.id;
